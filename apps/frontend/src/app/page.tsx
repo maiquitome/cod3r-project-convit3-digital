6dd67eb44e664e0720302ab461c422c23fb67985
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-19 18:59:30 -0300

    refactor: :recycle: create `index.ts` files to change `import`

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,6 +1,8 @@
-import { PageTemplate } from "../components/template/PageTemplate";
+import { PageTemplate } from "@/src/components/template/PageTemplate";
+import { Evento } from "@/src/core";
 
 export default function Home() {
+  const e: Partial<Evento> = {};
   return (
     <PageTemplate>
       <div>Inicio</div>
---
+++ apps/frontend/src/core/evento/index.ts
@@ -0,0 +1,4 @@
+import Evento from "./models/Evento";
+import Convidado from "./models/Convidado";
+
+export type { Evento, Convidado };
