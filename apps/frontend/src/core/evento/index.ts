5f000d907be390f4a1b89272a34331cbd2fa1445
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-20 11:14:17 -0300

    feat: :sparkles: add `Id`

--- apps/frontend/src/core/evento/index.ts
+++ apps/frontend/src/core/evento/index.ts
@@ -1,4 +1,4 @@
 import Evento from "./models/Evento";
 import Convidado from "./models/Convidado";
 
-export type { Evento, Convidado };
+export type { Evento, Convidado }; // export `type` pois está sendo exportada `interface`, se fosse classe não precisaria do `type`
--- apps/frontend/src/core/shared/index.ts
+++ apps/frontend/src/core/shared/index.ts
@@ -1,1 +1,2 @@
-export {};
+import Id from "./Id";
+export { Id }; // Aqui não precisa de export `type` pois `Id` é uma classe e não uma `interface`
