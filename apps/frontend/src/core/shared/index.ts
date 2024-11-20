047e633c66e00e63fb631351768aa39145b5a436
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-20 13:19:20 -0300

    feat: :sparkles: add `Data.ts`

--- apps/frontend/src/core/shared/index.ts
+++ apps/frontend/src/core/shared/index.ts
@@ -1,5 +1,6 @@
 import Alias from "./Alias";
 import Id from "./Id";
 import Senha from "./Senha";
+import Data from "./Data";
 
-export { Alias, Id, Senha }; // Aqui não precisa de export `type` pois `Id` é uma classe e não uma `interface`
+export { Alias, Id, Senha, Data }; // Aqui não precisa de export `type` pois `Id` é uma classe e não uma `interface`
