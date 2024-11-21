77a782f85ca5c478909ca2ca76f926d10841c590
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-20 22:04:42 -0300

    feat: :sparkles: add `export` of `functions` to `src/core/evento/index.ts`

--- apps/frontend/src/core/evento/index.ts
+++ apps/frontend/src/core/evento/index.ts
@@ -1,4 +1,16 @@
 import Evento from "./models/Evento";
 import Convidado from "./models/Convidado";
 
+import complementarConvidado from "./functions/complementarConvidado";
+import complementarEvento from "./functions/complementarEvento";
+import criarConvidadoVazio from "./functions/criarConvidadoVazio";
+import criarEventoVazio from "./functions/criarEventoVazio";
+
 export type { Evento, Convidado }; // export `type` pois está sendo exportada `interface`, se fosse classe não precisaria do `type`
+
+export {
+  complementarConvidado,
+  complementarEvento,
+  criarConvidadoVazio,
+  criarEventoVazio,
+};
