8474e2f540810ff0ccbd52b40a901016403a7b2a
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-23 14:59:00 -0300

    feat: :sparkles: add `AcessarViaQrCode`

--- apps/frontend/src/components/evento/DashboardEvento.tsx
+++ apps/frontend/src/components/evento/DashboardEvento.tsx
@@ -1,5 +1,6 @@
 import { Evento } from "@/src/core";
 import InformacoesEvento from "./InformacoesEvento";
+import AcessarViaQrCode from "./AcessarViaQrCode";
 
 export interface DashboardEventoProps {
   evento: Evento;
@@ -8,8 +9,9 @@ export interface DashboardEventoProps {
 export default function DashboardEvento(props: DashboardEventoProps) {
   return (
     <div className="flex flex-col gap-2">
-      <div className="flex gap-2">
+      <div className="flex gap-2 self-stretch">
         <InformacoesEvento evento={props.evento} className="flex-1" />
+        <AcessarViaQrCode evento={props.evento} />
       </div>
     </div>
   );
