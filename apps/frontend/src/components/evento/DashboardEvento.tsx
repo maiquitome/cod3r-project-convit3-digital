35e3daf0365585fdf9dbb4600e2354caa1ab0981
Maiqui Pirolli Tomé <maiquitome@gmail.com>
2024-11-23 10:43:35 -0300

    feat: :sparkles: add `Informacao.tsx`

--- apps/frontend/src/components/evento/DashboardEvento.tsx
+++ apps/frontend/src/components/evento/DashboardEvento.tsx
@@ -7,9 +7,9 @@ export interface DashboardEventoProps {
 
 export default function DashboardEvento(props: DashboardEventoProps) {
   return (
-    <div>
-      <div>
-        <InformacoesEvento evento={props.evento} />
+    <div className="flex flex-col gap-2">
+      <div className="flex gap-2">
+        <InformacoesEvento evento={props.evento} className="flex-1" />
       </div>
     </div>
   );
